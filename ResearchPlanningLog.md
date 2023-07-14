d37345df3569b618a000d2efa1600bd23651d662
Laura Hope <ssteamlaura@gmail.com>
2023-07-14 13:35:28 -0700

    Add basic info with import statements for Navbar.js

--- ResearchPlanningLog.md
+++ ResearchPlanningLog.md
@@ -11,3 +11,4 @@
 * 8:00-9:00: Changing over project info from Bob's Burgers to Personal Portfolio
 * 9:15: Researching various tools for portfolio
 * 10:00-11:30: Continued research, create component diagram
+* 1:30: Started component basics
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Skills } from './Skills';
+import { AboutMe } from './AboutMe';
+import { Projects } from './Projects';
+import { Contact } from './Contact';
+
+export const Navbar = () => {
+  
+}
