e12c37539b086cab5c55d5e41b6d6e90a725df22
Laura Hope <ssteamlaura@gmail.com>
2023-07-14 16:36:51 -0700

    Add sections (divs, container, col, rows) to AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,8 +1,57 @@
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export const AboutMe = () => {
 
   return (
-
-  )
+    <div className="intro">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} s={8} md={6}>
+            <div className="about-me">
+              <h1>About Me</h1>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+            </div>
+          </Col>
+          <Col xs={12} s={8} md={6}>
+            <img src="https://via.placeholder.com/300" alt="placeholder" />
+          </Col>
+          <Col xs={12} s={8} md={6}>
+            <div className="education">
+              <h1>Education</h1>
+              <p></p>
+              <p></p>
+              <p></p>
+            </div>
+          </Col>
+          <Col xs={12} s={8} md={6}>
+            <div className="skills">
+              <h1>Skills</h1>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+            </div>
+          </Col>
+          <Col xs={12} s={8} md={6}>
+            <div className="hobbies">
+              <h1>Hobbies</h1>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar } from './Navbar';
 import { Banner } from './Banner';
 import { AboutMe } from './AboutMe';
-import { Skills } from './Skills';
 import { Projects } from './Projects';
 import { Contact } from './Contact';
 import { Footer } from './Footer';
@@ -14,7 +13,6 @@ function App() {
       <Navbar />
       <Banner />
       <AboutMe />
-      <Skills />
       <Projects />
       <Contact />
       <Footer />
