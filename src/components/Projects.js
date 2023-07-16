867a2897d893259244b885b5f67b1f839e80bbbd
Laura Hope <ssteamlaura@gmail.com>
2023-07-15 19:55:41 -0700

    Add initial projects.js info with layout for 1 JS project

---
+++ src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './App.css';
+
+export const Projects = () => {
+  const javascriptProjects = [
+    {
+      name: '',
+      description: '',
+      technologies: '',
+      github: (
+
+      ),
+      image: "",
+    },
