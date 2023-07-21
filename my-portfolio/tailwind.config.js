02faaeab8e8c2efac6b6643ee11250079f6cb74e
Laura Hope <ssteamlaura@gmail.com>
2023-07-21 12:20:32 -0700

    Add pages directory

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,18 +1,21 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      transitionDuration: {
+        0: "0ms",
+        2000: "2000ms",
+        4000: "4000ms",
       },
     },
   },
+  variants: {
+    extend: {},
+    display: ["responsive", "group-hover", "group-focus"],
+  },
+
   plugins: [],
-}
+};
