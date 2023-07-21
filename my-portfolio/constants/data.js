893c48acdfc7c8523b100396b3d442d839aefd1b
Laura Hope <ssteamlaura@gmail.com>
2023-07-21 13:23:41 -0700

    Add basic userData info to constants directory

---
+++ my-portfolio/constants/data.js
@@ -0,0 +1,9 @@
+const userData = {
+  githubUsername: "lauramhope",
+  name: "Laura Hope",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "lauramhope.dpt@gmail.com",
+  phone: "404.819.7311",
+  address: "Seattle, WA, USA"
+}
