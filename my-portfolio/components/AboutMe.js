18a47240fa979d5d739e5d3563ad63d8c3545456
Laura Hope <ssteamlaura@gmail.com>
2023-07-24 08:29:57 -0700

    Add AboutMe section and finish userData info

---
+++ my-portfolio/components/AboutMe.js
@@ -0,0 +1,166 @@
+import React from "react";
+import userData from "@constants/data";
+
+export default function AboutMe() {
+  return (
+    <section className="bg-white dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+          About Me.
+        </h1>
+      </div>
+      <div className="bg-[#F1F1F1] -mt-10 dark:bg-gray-900">
+        <div className="text-container max-w-6xl mx-auto pt-20">
+          <p
+            className="leading-loose text-2xl md:text-4xl font-semibold  mx-4"
+            style={{ lineHeight: "3rem" }}
+          >
+            {userData.about.title}
+          </p>
+        </div>
+      </div>
+      <div className="bg-[#F1F1F1] dark:bg-gray-900 px-4">
+        <div className="pt-20 grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-y-20 gap-x-20">
+          {/* Social Buttons */}
+          <div className="inline-flex flex-col">
+            <div>
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Contact
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                For questions, send me an{" "}
+                <a
+                  href={`mailto:${userData.email}`}
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                >
+                  email
+                </a>{" "}
+              </p>
+            </div>
+            <div className="mt-8">
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Job Opportunities
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                Actively seeking new opportunities. <br/>
+                If you find me to be a good fit,
+                take a moment to check out my{" "}
+                <a
+                  href={userData.resumeUrl}
+                  target="__blank"
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                >
+                  CV
+                </a>{"."}
+                <br/>
+                I'd love to join your team.
+              </p>
+            </div>
+            {/* Social Links */}
+            <h1 className="text-xl font-semibold text-gray-700 mt-8 dark:text-gray-200">
+              Social Links
+            </h1>
+            <div className="mt-4 ml-4">
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.github}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    GitHub
+                  </div>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.linkedin}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    LinkedIn
+                  </div>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.instagram}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-28 group-hover:translate-x-0 transition duration-300"></div>
+                    Instagram
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Text area */}
+          <div className="col-span-1 md:col-span-2">
+            {userData.about.description?.map((desc, idx) => (
+              <p
+                key={idx}
+                className="text-xl text-gray-700 mb-4 dark:text-gray-300 "
+              >
+                {desc}
+              </p>
+            ))}
+
+            <hr />
+            <br />
+            
+            <h1 className="bg-blue-800 text-3xl rounded-md px-2 py-1 inline-block font-bold text-gray-50">
+              Technologies Used:
+            </h1>
+            <div className="flex flex-row flex-wrap mt-8">
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img 
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/csharp/csharp.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/firebase/firebase.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mysql/mysql.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img 
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/terminal/terminal.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- my-portfolio/constants/data.js
+++ my-portfolio/constants/data.js
@@ -48,4 +48,46 @@ const userData = {
       imgUrl: "/wordle.png",
     },
   ],
-}
+  about: {
+    title:
+      "I'm a full-stack junior web developer with an enthusiam for creating and an eagerness to learn.",
+    description: [
+      `I'm a highly motivated individual with a strong problem-solving mindset, driven by a fervent passion for both continuous learning and innovative creations. I'm a recent graduate of Epicodus' full-stack development program with an extensive background in patient care. I'm excited to bring my skills in communication, teamwork, and problem solving to a new career in software development.`,
+      `Before Epicodus, I was a practicing Physical Therapist where I assisted in the physical rehabilitation of people with various pathologies. During my time as a Physical Therapist, I provided excellence in patient care via utilizing my interpersonal skills and facilitating movement to optimize patient independence. While my time as a Physical Therapist was rewarding for my personal growth, I am pleased to have made the transition over to another field that will challenge me to problem-solve independent of patient care. 
+      `,
+      `Currently, I'm focused on refining my skills in React, C#, and SQL. I'm eagerly looking forward to furthering my growth as a developer. Throughout my time at Epicodus, I have successfully completed numerous projects, ranging from simple word games to full-stack web applications. The experience has been immensely rewarding, and I am enthusiastic about the prospect of expanding my skillset and taking on more ambitious projects in the future.`,
+    ],
+  },
+  education: [
+    {
+      diploma: "Full-stack Software Developer",
+      program: "Epicodus",
+      year: "2023",
+      desc: "Completed a 27-week full-time program focused on C#, .NET, React, and SQL.",
+      link: "https://www.epicodus.com/",
+    },
+    {
+      diploma: "Doctor of Physical Therapy",
+      program: "Georgia State University",
+      year: "2021",
+      desc: "Completed a 3-year program focused on patient care and rehabilitation.",
+      link: "https://www.gsu.edu/",
+    },
+    {
+      diploma: "Bachelor of Science in Exercise Science",
+      program: "Georgia State University",
+      year: "2018",
+      desc: "Completed a 4-year program focused on human anatomy and physiology.",
+      link: "https://www.gsu.edu/",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1MFlY3KRhHeNWUSk-zIyHiNaw2Bsj6bPg/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/lauramarie43",
+    linkedin: "https://www.linkedin.com/in/laura-hope-softwaredev/",
+    github: "https://github.com/lauramhope",
+  },
+};
+
+export default userData;
