ce5483b3e27feda20afa2c71a3812a01a9144762
Laura Hope <ssteamlaura@gmail.com>
2023-07-24 12:40:50 -0700

    Add contact section info

--- my-portfolio/components/AboutMe.js
+++ my-portfolio/components/AboutMe.js
@@ -3,13 +3,13 @@ import userData from "@constants/data";
 
 export default function AboutMe() {
   return (
-    <section className="bg-white dark:bg-gray-800">
-      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+    <section className="bg-blue-100 dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-blue-100 dark:bg-gray-800">
         <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
           About Me.
         </h1>
       </div>
-      <div className="bg-[#F1F1F1] -mt-10 dark:bg-gray-900">
+      <div className="bg-blue-200 -mt-10 dark:bg-gray-900">
         <div className="text-container max-w-6xl mx-auto pt-20">
           <p
             className="leading-loose text-2xl md:text-4xl font-semibold  mx-4"
