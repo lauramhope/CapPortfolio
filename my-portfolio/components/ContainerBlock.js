9508e42878d3cc6e17d8c3f28baaba5d99e1888c
Laura Hope <ssteamlaura@gmail.com>
2023-07-24 13:37:58 -0700

    Add containerblock section

---
+++ my-portfolio/components/ContainerBlock.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function ContainerBlock({ children, ...customMeta }) {
+  const router = useRouter();
+
+  const meta = {
+    title: "Laura Hope - Jr. Web Developer",
+    description: `Graduate from Epicodus Full-Stack Software Development Program. Get in touch with me to know more.`,
+    image: "/avatar.jpg",
+    type: "website",
+    ...customMeta,
+  };
+  return (
+    <div>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Laura Hope" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>
+      <main className="dark:bg-gray-800 w-full">
+        <Navbar />
+        <div>{children}</div>
+        <Footer />
+      </main>
+    </div>
+  );
+}
