0f7de97058c9436881cb8bf984c57dee9fd36ced
Laura Hope <ssteamlaura@gmail.com>
2023-07-24 08:24:36 -0700

    Add projects section to data.js

--- my-portfolio/constants/data.js
+++ my-portfolio/constants/data.js
@@ -5,5 +5,47 @@ const userData = {
   avatarUrl: "/avatar.jpg",
   email: "lauramhope.dpt@gmail.com",
   phone: "404.819.7311",
-  address: "Seattle, WA, USA"
+  address: "Seattle, WA, USA",
+  projects: [
+    {
+      title: "Pizza Parlor",
+      link: "https://github.com/lauramhope/pizza-parlor.git",
+      imgUrl: "/pizzaparlorJS.png",
+    },
+    {
+      title: "Bike API",
+      link: "https://github.com/lauramhope/bike-API.git",
+      imgUrl: "/bikeAPIteam.png",
+    },
+    {
+      title: "Dr. Sillystringz's Factory",
+      link: "https://github.com/lauramhope/DrSillyStringz.Solution.git",
+      imgUrl: "/factory.png",
+    },
+    {
+      title: "Pierre's Sweet and Savory Treats",
+      link: "https://github.com/lauramhope/PierresSweetSavoryTreats.Solution.git",
+      imgUrl: "/bakery.png",
+    },
+    {
+      title: "BeanVentory",
+      link: "https://github.com/lauramhope/BeanVentory.git",
+      imgUrl: "/coffee.png",
+    },
+    {
+      title: "Farmer's Market",
+      link: "https://github.com/lauramhope/farmers-market.git",
+      imgUrl: "/market.png",
+    },
+    {
+      title: "The One API",
+      link: "https://github.com/lauramhope/The_One_API.Solution.git",
+      imgUrl: "/theoneAPI.png",
+    },
+    {
+      title: "Wordle App",
+      link: "https://github.com/lauramhope/wordle-app.git",
+      imgUrl: "/wordle.png",
+    },
+  ],
 }
