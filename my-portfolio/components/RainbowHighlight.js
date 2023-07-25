2c1416e715ff06cf4e42e273466270acefe978d5
Laura Hope <ssteamlaura@gmail.com>
2023-07-24 18:03:13 -0700

    Add rainbowhighlight section

---
+++ my-portfolio/components/RainbowHighlight.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { RoughNotation } from "react-rough-notation";
+
+export const RainbowHighlight = ({ color, children }) => {
+  const animationDuration = Math.floor(30 * children.length);
+
+  return (
+    <RoughNotation
+      type="highlight"
+      multiline={true}
+      padding={[0, 2]}
+      iterations={1}
+      animationDuration={animationDuration}
+      color={color}
+    >
+      {children}
+    </RoughNotation>
+  );
+};
