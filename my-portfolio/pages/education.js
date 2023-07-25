ebbb2901dd96f179881f99cb26ccb21395d5a6ea
Laura Hope <ssteamlaura@gmail.com>
2023-07-24 22:14:26 -0700

    Add education section to pages

---
+++ my-portfolio/pages/education.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Education from "../components/Education";
+
+export default function education() {
+  return (
+    <ContainerBlock title="Education - Laura Hope">
+      <Education />
+    </ContainerBlock>
+  );
+}
