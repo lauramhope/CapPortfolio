aff1c3ccf6c59c7aebba0d4fce6a6e3603ca56f8
Laura Hope <ssteamlaura@gmail.com>
2023-07-25 11:55:50 -0700

    add small changes

--- my-portfolio/components/FavProjects.js
+++ my-portfolio/components/FavProjects.js
@@ -28,7 +28,7 @@ export default function FavProjects() {
                 d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"
               />
             </svg>
-            <p>View all</p>
+            <p>View more</p>
           </Link>
         </header>
         <div className="grid md:grid-cols-3 gap-8 lg:-mt-8 pb-40">
@@ -60,7 +60,7 @@ export default function FavProjects() {
                 alt="bike"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
-              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-blue-500 rounded-md px-2">
                 Bike API
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
