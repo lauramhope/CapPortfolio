c4774cbfa7366e0e9d8bcac2368277babbfb4eae
Laura Hope <ssteamlaura@gmail.com>
2023-07-25 20:18:02 -0700

    Add changes to education section for both light and dark modes

--- my-portfolio/components/Education.js
+++ my-portfolio/components/Education.js
@@ -3,15 +3,14 @@ import React from "react";
 
 export default function Education() {
   return (
-    <section className="bg-blue-100 dark:bg-gray-800">
-      <div className="max-w-6xl mx-auto h-48 bg-blue-100 dark:bg-gray-800">
+    <section className="bg-blue-100 dark:bg-gray-700">
+      <div className="max-w-6xl mx-auto h-48 bg-blue-100 dark:bg-gray-700">
         <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
           Education
         </h1>
       </div>
-      <div className="bg-white dark:bg-grey-900 -mt-4">
-        <div className="grid grid-cols-1 dark:bg-gray-900 max-w-xl mx-auto pt-20">
-          {/* Experience card */}
+      <div className="bg-gray-100 dark:bg-gray-700 -mt-4">
+        <div className="grid grid-cols-1 dark:bg-gray-500 max-w-xl mx-auto pt-20">
           {userData.education.map((exp, idx) => (
             <>
               <EducationCard
@@ -27,7 +26,7 @@ export default function Education() {
                   <div className="w-4 h-4 bg-pink-500 rounded-full relative z-10">
                     <div className="w-4 h-4 bg-pink-500 rounded-full relative z-10 animate-ping"></div>
                   </div>
-                  <div className="w-1 h-24 bg-grey-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+                  <div className="w-1 h-24 bg-gray-400 dark:bg-gray-700 rounded-full -mt-2"></div>
                 </div>
               )}
             </>
@@ -41,7 +40,7 @@ export default function Education() {
 const EducationCard = ({ diploma, desc, year, program, link }) => {
   return (
     <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4">
-      <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-800">
+      <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-400 font-bold dark:text-gray-800">
         {year}
       </h1>
       <h1 className="font-semibold text-xl">{diploma}</h1>
--- my-portfolio/constants/data.js
+++ my-portfolio/constants/data.js
@@ -1,7 +1,7 @@
 const userData = {
   githubUsername: "lauramhope",
   name: "Laura Hope",
-  designation: "Full-Stack Developer",
+  designation: "Jr Full-Stack Developer",
   avatarUrl: "/avatar.jpg",
   email: "lauramhope.dpt@gmail.com",
   phone: "404.819.7311",
